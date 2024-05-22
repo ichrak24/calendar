38195d0c4a3b1b7c5d69c1e33d4a028f897cbbd9
ichrak guesmi <guesmiichrak3@gmail.com>
2024-05-22 22:55:56 +0100

    calendar symfony

---
+++ src/Controller/ReservationController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Reservation;
+use App\Form\ReservationType;
+use App\Repository\ReservationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/reservation')]
+class ReservationController extends AbstractController
+{
+    #[Route('/', name: 'reservation_index', methods: ['GET'])]
+    public function index(ReservationRepository $reservationRepository): Response
+    {
+        return $this->render('reservation/index.html.twig', [
+            'reservations' => $reservationRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/calendar', name: 'reservation_calendar', methods: ['GET'])]
+    public function calendar(ReservationRepository $reservationRepository): Response
+    {
+        return $this->render('reservation/calendar.html.twig', [
+            'reservations' => $reservationRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'reservation_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $reservation = new Reservation();
+        $form = $this->createForm(ReservationType::class, $reservation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($reservation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('reservation_index');
+        }
+
+        return $this->render('reservation/new.html.twig', [
+            'reservation' => $reservation,
+            'form' => $form->createView(),
+        ]);
+    }
+}
